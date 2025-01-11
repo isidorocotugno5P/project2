31d6fc89874b5509501fb66debf10aaeeb5f7dad
isidorocotugno5P <l1sb03u4@students.codeinstitute.net>
2025-01-11 16:11:35 +0000

    Add obstacle generator and movement of obstacle to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 
+const gameArea = document.getElementById("game-area");
 const character = document.getElementById("character");
-const obstacle = document.getElementById("obstacle");
+// const obstacle = document.getElementById("obstacle");
 const score = document.getElementById("score");
 document.addEventListener('keydown', controls);
 let characterPosition = 0
@@ -8,7 +9,6 @@ let characterPosition = 0
 function controls(e) {
     if (e.code === 'Space') {
         jump();
-        console.log('waddup');
     }
 }
 
@@ -19,6 +19,30 @@ function jump() {
         character.classList.remove("jump");
     }, 700);
 }
+
+function createObstacles () {
+    let randomNumber = Math.random() * 6000;
+    const obstacleCreation = document.createElement('div');
+    obstacleCreation.classList.add('obstacle');
+    gameArea.appendChild(obstacleCreation);
+    let obstacleRight = -76
+
+    obstacleMover()
+
+    function obstacleMover() {    
+        setInterval(function(){
+            obstacleRight += 1
+            obstacleCreation.style.right = obstacleRight + 'px'
+        }, 0.5)
+
+        setTimeout(createObstacles, randomNumber)
+        if (obstacleCreation.style.right == '500px')  {
+            gameArea.appendChild(obstacleCreation);
+        }
+    }
+}
+
+createObstacles ()
 
 setInterval(() => {
     score.innerText++;
