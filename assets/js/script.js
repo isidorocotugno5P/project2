db60fbeaa66312085d9e12f8beff1497daae847d
Isidoro Cotugno <l1sb03u4@students.codeinstitute.net>
2025-01-14 16:50:59 +0000

    Add walk and jump animations to character

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,29 @@
-
 const gameArea = document.getElementById("game-area");
 const character = document.getElementById("character");
 const score = document.getElementById("score");
 let result = document.getElementById("result");
 const start = document.getElementById("start");
 const endGame = document.getElementById("end-game");
+const title = document.getElementById("game-title");
 document.addEventListener('keydown', controls);
 let characterPosition = 0;
 
 function controls(e) {
     if (e.code === 'Space') {
-        start.classList.add('hide')
+        start.classList.add('hide');
+        character.classList.remove('walk');
         jump();
     }
 }
 
 function jump() {
-    if (!character.classList == "jump") { return }
+    if (!character.classList == "jump") {
+        return;
+    }
     character.classList.add("jump");
-    setTimeout(function () {
+    setTimeout(() => {
         character.classList.remove("jump");
+        character.classList.add('walk');
     }, 700);
 }
 
@@ -33,19 +37,20 @@ function createObstacles() {
     obstacleMover()
 
     function obstacleMover() {
-        setInterval(function () {
-            obstacleRight += 1
-            obstacleCreation.style.right = obstacleRight + 'px'
+        setInterval(() => {
+            obstacleRight += 1;
+            obstacleCreation.style.right = obstacleRight + 'px';
             let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-            let screenwidth = window.screen.width
-            let percentage = Math.floor((obstacleRight*100)/screenwidth)
-            console.log(percentage)
+            let screenwidth = window.screen.width;
+            let percentage = Math.floor((obstacleRight * 100) / screenwidth);
+            // console.log(percentage);
             if (percentage > 94 && percentage < 99 && characterTop >= 240) {
-            // alert("Game Over. score: " + score.innerText);
-            endGame.classList.add('end')
-            result.innerText = score.innerText
+                // alert("Game Over. score: " + score.innerText);
+                endGame.classList.add('end');
+                document.getElementById("result").style.fontSize = "xx-large";
+                result.innerText = score.innerText;
             }
-        }, 0.5)
+        }, 0.5);
 
         setTimeout(createObstacles, randomNumber)
         if (obstacleCreation.style.right == '500px') {
